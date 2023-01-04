d362c297be53099652be3a2ce5b64014d5fdc83e
Adrien Delacroix <57764513+Delacroix-75020@users.noreply.github.com>
2023-01-04 11:55:54 +0100

    Ajout class card

---
+++ card.py
@@ -0,0 +1,11 @@
+class Card:
+    def __init__(self, id, name, type, rarity, cost, attack, health, text, mechanics):
+        self.id = id
+        self.name = name
+        self.type = type
+        self.rarity = rarity
+        self.cost = cost
+        self.attack = attack
+        self.health = health
+        self.text = text
+        self.mechanics = mechanics
