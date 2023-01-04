93f47234e1192c848e1f0b007850aae46906b8cb
AntouaneV <93910931+AntouaneV@users.noreply.github.com>
2023-01-04 11:38:47 +0100

    ADD react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Shop from './pages/Shop';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/shop',
+    element: <Shop />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
