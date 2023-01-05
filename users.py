324a6ea0e2e467d7caff80c2aa3634e032131509
Muthuvel <muthuvel.savoundirapandiane@outlook.com>
2023-01-05 17:44:16 +0100

    wip

---
+++ users.py
@@ -0,0 +1,18 @@
+from dbConnect import connectDB
+    
+    
+def InsertUser():
+
+    db = connectDB()
+
+    mySql_select_query = """SELECT * FROM users"""
+
+    cursor = db.cursor()
+
+    cursor.execute(mySql_select_query)
+
+    records = cursor.fetchall()
+
+    return records    
+
+
