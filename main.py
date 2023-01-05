9654c7becf2d7232657add9ebe63a45c415efbde
Damien SALEM <d.salem@ecole-ipssi.net>
2023-01-05 14:22:21 +0100

    modif main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import os
+from entity.game import Game
 
 app = FastAPI()
 
@@ -7,6 +8,11 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/")
+async def start_game():
+    game= Game()
+    return game.
 
 if __name__=="__main__":
     os.system("uvicorn main:app --reload")
