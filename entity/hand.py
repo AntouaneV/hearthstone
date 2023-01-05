27637327d098cfc993f6114d2693d42d33e087d9
Dwena <aatia1999@gmail.com>
2023-01-05 16:22:31 +0100

    je sais pas

--- entity/hand.py
+++ entity/hand.py
@@ -1,15 +1,15 @@
 from card import Card
-class hand:
-    def __init__(self):
-        self.hand = []
-        self.cards = [Card(id, type)]
 
-    # methode pour ajouter liste de cartes
-    def add_multiple_card(self,card_list):
-        self.cards.extend(card_list)
-    # methode pour ajouter 1 seul carte
-    def add_card(self,card):
+# methode pour ajouter des cartes
+def add_card(self,card):
+    if type(card) == list :
+        self.cards.extend(card)
+    else :
         self.cards.append(card)
     # methode pour supprimer 1 carte joué
-    def delete_card(self,card):
-        self.cards.remove(card)
+def delete_card(self,card):
+    self.cards.remove(card)
+class hand:
+    def __init__(self,cards):
+        self.hand = []
+        self.cards = add_card(cards)
