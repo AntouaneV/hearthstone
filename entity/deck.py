eb3bfc1eca5a183559b1dfe1a86f8a1407814c94
Damien SALEM <d.salem@ecole-ipssi.net>
2023-01-05 20:28:22 +0100

    updates entity, data and main

--- entity/deck.py
+++ entity/deck.py
@@ -1,11 +1,14 @@
 import random
 from card import Card
+from typing import List
 
 
 class Deck:
-    def __init__(self):
+    cardlist:List[Card]
+    def __init__(self,deck:list):
         # Initialise la liste de cartes avec une instance de Card pour chaque combinaison de valeur et de symbole
-        self.cards = [Card(id, type)]
+        for card in deck:
+            self.cardlist.append(Card(card))
         # ID et Type sont deux listes qui contiennent les valeurs et les symboles possibles pour les cartes d'un jeu de cartes standard
 
     def shuffle(self):
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 import os
 from entity.game import Game
-
+import jinja2
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import json
 app = FastAPI()
 
 
@@ -11,13 +15,27 @@ async def root():
 
 
 @app.post("/game")
-async def start_game(user):
+async def start_game():
+    testfile = open("data/deck.json", "r")
+    user = {
+        "id": 6,
+        "name": "Toto",
+        "hero": {
+            "name": "Jaina Proudmore",
+            "power": {
+                "name": "fireball",
+                "description": "inflige 1 point de dégâts à n'importe quelle cible."
+            }
+        },
+        "deck": json.load(testfile)
+    }
     game = Game(user)
     return game.id
 
 
 @app.get("/game/{id}")
 async def find_game(id):
+    # affichage sur un template html de la partie
     return {"id": id}
 
 if __name__ == "__main__":
