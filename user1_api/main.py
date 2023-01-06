a8ebd867e82d1360291d8c0eab7977984c02361a
Muthuvel <muthuvel.savoundirapandiane@outlook.com>
2023-01-06 10:48:35 +0100

    wip

---
+++ user1_api/main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI,Request
+import os
+from typing import Union
+from pydantic import BaseModel
+import users as u
+
+    
+app = FastAPI()
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/users")
+async def users_info():
+    json_reponse = []
+    data = u.InsertUser()
+    for user in data:
+        response = {
+            'user_id':user[0],
+            'username':user[1],
+            'Email':user[2],
+            'Password':user[3],
+            'Nb_games_played':user[4],
+            'Nb_games_won':user[5],
+            'Nb_unit_played':user[6],
+            'Nb_hero_power':user[7],
+            'Nb_spells_played':user[8],
+            'Nb_gold':user[8],
+            'Nb_arcone_dust':user[9],
+            'Nb_xp':user[10]
+        }
+        json_reponse.append(response)
+    return json_reponse
+  
+    
+
+# @app.get("/users/{id}")
+# async def login(id):
+#     return {"id": id, "mot de pass": 'password'}
+@app.post("/users/create")
+async def create_users(info : Request):      
+        request_data = await info.json()
+        usernamedata = request_data['username']
+        passworddata = request_data['password']
+        emaildata = request_data['email']
+        u.create_user(usernamedata,passworddata,emaildata)
+        return '''
+           username: {}
+           password: {}
+           email: {}'''.format(usernamedata,passworddata,emaildata)    
+        
+        
+if __name__=="__main__":
+    os.system("uvicorn main:app --reload")
---
+++ user1_api/users.py
@@ -0,0 +1,18 @@
+from dbConnect import connectDB
+    
+    
+def InsertUser():
+    db = connectDB()
+    mySql_select_query = """SELECT * FROM users"""
+    cursor = db.cursor()
+    cursor.execute(mySql_select_query)
+    records = cursor.fetchall()
+    return records    
+
+def create_user(username, password, email):
+    db = connectDB()
+    insert_query = "INSERT INTO users (username, password,email) VALUES (%s, %s, %s)"
+    cursor = db.cursor()
+    cursor.execute(insert_query, (username, password, email))
+    db.commit()
+    cursor.close()
